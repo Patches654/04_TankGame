06fd2ff1a7985f1c04868b548c9029b43dd50aa5
Ben Houlihan <benjamin.houlihan@outlook.com>
2017-05-22 20:36:21 +1000

    Ray Trace Setup

--- BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -24,4 +24,6 @@ public:
 	// Start moving the barrel so that a shot would 
 	// hit where the crosshair intersects the world
 	void AimTowardsCrosshair();
+
+	bool GetSightRayHitLocation(FVector& HitLocation) const;
 };
